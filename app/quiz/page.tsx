0e6a5c30c93d9d30c350e71f18ce9a55a36dcf56
mentorvikash <mentorvikash@gmail.com>
2025-04-05 01:07:52 +0530

    button box width

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -267,7 +267,7 @@ export default function Home() {
                 Submit
               </button>
             ) : (
-              <div className="flex w-1/3 justify-between">
+              <div className="flex w-1/3 sm:w-1/2 justify-between">
                 <button
                   type="button"
                   className=" align-middle text-black bg-white md:text-xl px-4 py-2 border-1 font-bold rounded-xl shadow-xl hover:border-0 sm:text-xs"
